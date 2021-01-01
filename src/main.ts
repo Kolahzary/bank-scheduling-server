6b0dcf80285bc9b6e189d812f179d7f7135cd0bd
Shadman Kolahzary <shadman.ko@gmail.com>
2021-01-02 01:07:14 +0330

    fix: run http on port 3000

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SOCKET_BIND_PORT } from './environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(SOCKET_BIND_PORT);
+  await app.listen(3000);
 }
 bootstrap();
